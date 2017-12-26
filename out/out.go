c42e4badf73580caf3b3c35fb3b36a1de3b0385f
Alexey Sudachen <alexey@sudachen.name>
2017-12-26 14:19:51 -0300

    updated

--- out/out.go
+++ out/out.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"io"
+	"bytes"
 )
 
 type Level byte
@@ -12,46 +13,88 @@ const (
 	Error Level = iota
 	Warn
 	Info
+	Verbose
 	Debug
 	Trace
+	levelsCount
 )
 
-var defaultOutput = os.Stderr
-var output = []io.Writer{}
+var DefaultWriter io.Writer = os.Stderr
+var PrintFunction func(lvl Level, b []byte) = DefaultPrintFunction
+var PrefixFunction func(lvl Level, bf* bytes.Buffer) = DefaultPrefixFunction
+
+var writer = make([]io.Writer,levelsCount)
+var prefix = make([][]byte,levelsCount)
 var currentLevel = Info
 
-func (v Level) SetOutput(wr io.Writer) {
-	output[v] = wr
+func init() {
+	Error.SetPrefix("error")
+	Warn.SetPrefix("warn")
+	Debug.SetPrefix("debug")
+	Trace.SetPrefix("trace")
 }
 
-func (v Level) SetCurrent() {
-	currentLevel = v
+func (lvl Level) SetWriter(wr io.Writer) {
+	writer[lvl] = wr
 }
 
-func (v Level) Visible() bool {
-	return v <= currentLevel
+func (lvl Level) SetCurrent() {
+	currentLevel = lvl
 }
 
-func (v Level) Output() io.Writer {
-	wr := output[v]
-	if wr == nil { return defaultOutput }
+func (lvl Level) Visible() bool {
+	return lvl <= currentLevel
+}
+
+func (lvl Level) Writer() io.Writer {
+	wr := writer[lvl]
+	if wr == nil { return DefaultWriter }
 	return wr
 }
 
-func (v Level) Println(a ...interface{}) {
-	if v.Visible() {
-		fmt.Fprintln(v.Output(), a...)
+func (lvl Level) Prefix() []byte {
+	return prefix[lvl]
+}
+
+func (lvl Level) SetPrefix(pfx string) {
+	prefix[lvl] = []byte(pfx)
+}
+
+var endl = []byte{'\n'}
+var pfxSep = []byte{':',' '}
+var textBuf bytes.Buffer
+
+func DefaultPrefixFunction(lvl Level, bf* bytes.Buffer) {
+	if pfx := lvl.Prefix(); pfx != nil {
+		bf.Write(pfx)
+		bf.Write(pfxSep)
 	}
 }
 
-func (v Level) Print(a ...interface{}) {
-	if v.Visible() {
-		fmt.Fprint(v.Output(), a...)
+func DefaultPrintFunction(lvl Level, b []byte) {
+	lvl.Writer().Write(b)
+}
+
+func (lvl Level) Print(a ...interface{}) {
+	if lvl.Visible() {
+		textBuf.Reset()
+		PrefixFunction(lvl,&textBuf)
+		fmt.Fprint(&textBuf, a...)
+		if l := textBuf.Len(); l == 0 || textBuf.Bytes()[l-1] != '\n' {
+			textBuf.Write(endl)
+		}
+		PrintFunction(lvl,textBuf.Bytes())
 	}
 }
 
-func (v Level) Printf(t string, a ...interface{}) {
-	if v.Visible() {
-		fmt.Fprintf(v.Output(), t, a...)
+func (lvl Level) Printf(t string, a ...interface{}) {
+	if lvl.Visible() {
+		textBuf.Reset()
+		PrefixFunction(lvl,&textBuf)
+		fmt.Fprintf(&textBuf, t, a...)
+		if l := textBuf.Len(); l == 0 || textBuf.Bytes()[l-1] != '\n' {
+			textBuf.Write(endl)
+		}
+		PrintFunction(lvl,textBuf.Bytes())
 	}
 }
